5e319ee9efad4876de454fae731579d4769d2c26
Lee Taylor <ltaylor@llnl.gov>
2017-12-10 13:59:49 -0800

    Update to 0.3.0

--- shroud/__init__.py
+++ shroud/__init__.py
@@ -42,6 +42,6 @@ Shroud - generate language bindings
 """
 
 
-__version__ = "0.2.0"
-version_info = (0,2,0,"beta",0)
+__version__ = "0.3.0"
+version_info = (0,3,0,"beta",0)
 # 'alpha', 'beta', 'candidate', or 'final'.
