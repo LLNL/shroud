88ca809009a0285c9a8faeed09aafbb7361d13f4
Lee Taylor <ltaylor@llnl.gov>
2018-02-23 18:42:51 -0800

    Update copyright

--- tests/tutorial/wrapClass1.h
+++ tests/tutorial/wrapClass1.h
@@ -1,7 +1,7 @@
 // wrapClass1.h
 // This is generated code, do not edit
 // #######################################################################
-// Copyright (c) 2017, Lawrence Livermore National Security, LLC.
+// Copyright (c) 2017-2018, Lawrence Livermore National Security, LLC.
 // Produced at the Lawrence Livermore National Laboratory
 //
 // LLNL-CODE-738041.
