51dc6170990e101abbb59af83f38e6444e868ae3
Lee Taylor <ltaylor@llnl.gov>
2019-02-28 19:28:54 -0800

    templates.yaml - set wrap_python: true

---
+++ regression/reference/templates/pytemplatesmodule.hpp
@@ -0,0 +1,102 @@
+// pytemplatesmodule.hpp
+// This is generated code, do not edit
+#ifndef PYTEMPLATESMODULE_HPP
+#define PYTEMPLATESMODULE_HPP
+#include <Python.h>
+// splicer begin header.include
+// splicer end header.include
+
+// forward declare classes
+namespace std {
+    class vector;
+}
+namespace std {
+    class vector;
+}
+class Worker;
+namespace internal {
+    class ImplWorker1;
+}
+class user;
+
+extern PyTypeObject PY_vector_int_Type;
+extern PyTypeObject PY_vector_double_Type;
+extern PyTypeObject PY_Worker_Type;
+extern PyTypeObject PY_ImplWorker1_Type;
+extern PyTypeObject PY_user_int_Type;
+
+// splicer begin header.C_declaration
+// splicer end header.C_declaration
+
+// helper functions
+extern const char *PY_vector_int_capsule_name;
+extern const char *PY_vector_double_capsule_name;
+extern const char *PY_Worker_capsule_name;
+extern const char *PY_ImplWorker1_capsule_name;
+extern const char *PY_user_int_capsule_name;
+PyObject *PP_vector_int_to_Object(std::vector_int *addr);
+int PP_vector_int_from_Object(PyObject *obj, void **addr);
+PyObject *PP_vector_double_to_Object(std::vector_double *addr);
+int PP_vector_double_from_Object(PyObject *obj, void **addr);
+PyObject *PP_Worker_to_Object(Worker *addr);
+int PP_Worker_from_Object(PyObject *obj, void **addr);
+PyObject *PP_ImplWorker1_to_Object(internal::ImplWorker1 *addr);
+int PP_ImplWorker1_from_Object(PyObject *obj, void **addr);
+PyObject *PP_user_int_to_Object(user_int *addr);
+int PP_user_int_from_Object(PyObject *obj, void **addr);
+
+// splicer begin class.vector.C_declaration
+// splicer end class.vector.C_declaration
+
+typedef struct {
+PyObject_HEAD
+    std::vector_int * obj;
+    // splicer begin class.vector.C_object
+    // splicer end class.vector.C_object
+} PY_vector_int;
+// splicer begin class.vector.C_declaration
+// splicer end class.vector.C_declaration
+
+typedef struct {
+PyObject_HEAD
+    std::vector_double * obj;
+    // splicer begin class.vector.C_object
+    // splicer end class.vector.C_object
+} PY_vector_double;
+// splicer begin class.Worker.C_declaration
+// splicer end class.Worker.C_declaration
+
+typedef struct {
+PyObject_HEAD
+    Worker * obj;
+    // splicer begin class.Worker.C_object
+    // splicer end class.Worker.C_object
+} PY_Worker;
+// splicer begin class.ImplWorker1.C_declaration
+// splicer end class.ImplWorker1.C_declaration
+
+typedef struct {
+PyObject_HEAD
+    internal::ImplWorker1 * obj;
+    // splicer begin class.ImplWorker1.C_object
+    // splicer end class.ImplWorker1.C_object
+} PY_ImplWorker1;
+// splicer begin class.user.C_declaration
+// splicer end class.user.C_declaration
+
+typedef struct {
+PyObject_HEAD
+    user_int * obj;
+    // splicer begin class.user.C_object
+    // splicer end class.user.C_object
+} PY_user_int;
+
+extern PyObject *PY_error_obj;
+
+#if PY_MAJOR_VERSION >= 3
+extern "C" PyMODINIT_FUNC PyInit_templates(void);
+#else
+extern "C" PyMODINIT_FUNC inittemplates(void);
+#endif
+
+#endif  /* PYTEMPLATESMODULE_HPP */
---
+++ regression/reference/templates/pyuser_inttype.cpp
@@ -0,0 +1,142 @@
+// pyuser_inttype.cpp
+// This is generated code, do not edit
+#include "pytemplatesmodule.hpp"
+#include "templates.hpp"
+// splicer begin class.user.impl.include
+// splicer end class.user.impl.include
+
+#ifdef __cplusplus
+#define SHROUD_UNUSED(param)
+#else
+#define SHROUD_UNUSED(param) param
+#endif
+
+#if PY_MAJOR_VERSION >= 3
+#define PyInt_AsLong PyLong_AsLong
+#define PyInt_FromLong PyLong_FromLong
+#define PyString_FromString PyUnicode_FromString
+#define PyString_FromStringAndSize PyUnicode_FromStringAndSize
+#endif
+// splicer begin class.user.impl.C_definition
+// splicer end class.user.impl.C_definition
+// splicer begin class.user.impl.additional_methods
+// splicer end class.user.impl.additional_methods
+static void
+PY_user_int_tp_del (PY_user_int *self)
+{
+// splicer begin class.user.type.del
+    delete self->obj;
+    self->obj = NULL;
+// splicer end class.user.type.del
+}
+
+static char PY_user_int_nested_double__doc__[] =
+"documentation"
+;
+
+static PyObject *
+PY_user_int_nested_double(
+  PY_user_int *self,
+  PyObject *args,
+  PyObject *kwds)
+{
+// splicer begin class.user.method.nested_double
+    int arg1;
+    double arg2;
+    const char *SHT_kwlist[] = {
+        "arg1",
+        "arg2",
+        NULL };
+
+    if (!PyArg_ParseTupleAndKeywords(args, kwds, "id:nested",
+        const_cast<char **>(SHT_kwlist), &arg1, &arg2))
+        return NULL;
+    self->obj->nested(arg1, arg2);
+    Py_RETURN_NONE;
+// splicer end class.user.method.nested_double
+}
+// splicer begin class.user.impl.after_methods
+// splicer end class.user.impl.after_methods
+static PyMethodDef PY_user_int_methods[] = {
+    {"nested_double", (PyCFunction)PY_user_int_nested_double,
+        METH_VARARGS|METH_KEYWORDS, PY_user_int_nested_double__doc__},
+    // splicer begin class.user.PyMethodDef
+    // splicer end class.user.PyMethodDef
+    {NULL,   (PyCFunction)NULL, 0, NULL}            /* sentinel */
+};
+
+static char user_int__doc__[] =
+"virtual class"
+;
+
+/* static */
+PyTypeObject PY_user_int_Type = {
+    PyVarObject_HEAD_INIT(NULL, 0)
+    "templates.user_int",                       /* tp_name */
+    sizeof(PY_user_int),         /* tp_basicsize */
+    0,                              /* tp_itemsize */
+    /* Methods to implement standard operations */
+    (destructor)0,                 /* tp_dealloc */
+    (printfunc)0,                   /* tp_print */
+    (getattrfunc)0,                 /* tp_getattr */
+    (setattrfunc)0,                 /* tp_setattr */
+#if PY_MAJOR_VERSION >= 3
+    0,                               /* tp_reserved */
+#else
+    (cmpfunc)0,                     /* tp_compare */
+#endif
+    (reprfunc)0,                    /* tp_repr */
+    /* Method suites for standard classes */
+    0,                              /* tp_as_number */
+    0,                              /* tp_as_sequence */
+    0,                              /* tp_as_mapping */
+    /* More standard operations (here for binary compatibility) */
+    (hashfunc)0,                    /* tp_hash */
+    (ternaryfunc)0,                 /* tp_call */
+    (reprfunc)0,                    /* tp_str */
+    (getattrofunc)0,                /* tp_getattro */
+    (setattrofunc)0,                /* tp_setattro */
+    /* Functions to access object as input/output buffer */
+    0,                              /* tp_as_buffer */
+    /* Flags to define presence of optional/expanded features */
+    Py_TPFLAGS_DEFAULT,             /* tp_flags */
+    user_int__doc__,         /* tp_doc */
+    /* Assigned meaning in release 2.0 */
+    /* call function for all accessible objects */
+    (traverseproc)0,                /* tp_traverse */
+    /* delete references to contained objects */
+    (inquiry)0,                     /* tp_clear */
+    /* Assigned meaning in release 2.1 */
+    /* rich comparisons */
+    (richcmpfunc)0,                 /* tp_richcompare */
+    /* weak reference enabler */
+    0,                              /* tp_weaklistoffset */
+    /* Added in release 2.2 */
+    /* Iterators */
+    (getiterfunc)0,                 /* tp_iter */
+    (iternextfunc)0,                /* tp_iternext */
+    /* Attribute descriptor and subclassing stuff */
+    PY_user_int_methods,                             /* tp_methods */
+    0,                              /* tp_members */
+    0,                             /* tp_getset */
+    0,                              /* tp_base */
+    0,                              /* tp_dict */
+    (descrgetfunc)0,                /* tp_descr_get */
+    (descrsetfunc)0,                /* tp_descr_set */
+    0,                              /* tp_dictoffset */
+    (initproc)0,                   /* tp_init */
+    (allocfunc)0,                  /* tp_alloc */
+    (newfunc)0,                    /* tp_new */
+    (freefunc)0,                   /* tp_free */
+    (inquiry)0,                     /* tp_is_gc */
+    0,                              /* tp_bases */
+    0,                              /* tp_mro */
+    0,                              /* tp_cache */
+    0,                              /* tp_subclasses */
+    0,                              /* tp_weaklist */
+    (destructor)PY_user_int_tp_del,                 /* tp_del */
+    0,                              /* tp_version_tag */
+#if PY_MAJOR_VERSION >= 3
+    (destructor)0,                  /* tp_finalize */
+#endif
+};
