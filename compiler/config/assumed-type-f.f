6dc1976a9041793f8dde6f9059c195a49c8fba92
Lee Taylor <ltaylor@llnl.gov>
2019-04-26 09:43:19 -0700

    Add a compiler directory to test compilers
    Add test for assumed-type: type(*)

---
+++ compiler/config/assumed-type-f.f
@@ -0,0 +1,31 @@
+      program tester
+        use, intrinsic :: iso_c_binding
+        implicit none
+        integer(C_SHORT) :: i_short
+        integer(C_INT)   :: i_int
+        integer(C_LONG)  :: i_long
+        integer(C_INT) :: rv1, rv2, rv3
+
+        interface
+           function c_func(a, flag) bind(C) result(rv)
+             use, intrinsic :: iso_c_binding
+             implicit none
+             type(*) :: a
+             integer(C_INT), value :: flag
+             integer(C_INT) :: rv
+           end function c_func
+        end interface
+
+        i_short = 2
+        i_int   = 4
+        i_long  = 8
+
+        rv1 = c_func(i_short, 1)
+        rv2 = c_func(i_int, 2)
+        rv3 = c_func(i_long, 3)
+        if (rv1 + rv2 + rv3 == 3) then
+           print *, "have_assumed_type"
+        else
+           print *, "no_assumed_type"
+        endif
+      end program tester
