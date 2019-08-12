9db763d17e563e63f9fecc78def00ad76311b719
Lee Taylor <ltaylor@llnl.gov>
2019-08-12 16:16:23 -0700

    Create a namespace in none.yaml
    This now creates a file wrapflibrary.f. But should it really?
    (It's just boilerplate)

---
+++ regression/reference/none/wrapflibrary.f
@@ -0,0 +1,36 @@
+! wrapflibrary.f
+! This is generated code, do not edit
+! #######################################################################
+! Copyright (c) 2017-2019, Lawrence Livermore National Security, LLC.
+!
+! Produced at the Lawrence Livermore National Laboratory
+!
+! LLNL-CODE-738041.
+!
+! All rights reserved.
+!
+! This file is part of Shroud.
+!
+! For details about use and distribution, please read LICENSE.
+!
+! #######################################################################
+!>
+!! \file wrapflibrary.f
+!! \brief Shroud generated wrapper for library library
+!<
+! splicer begin file_top
+! splicer end file_top
+module library_mod
+    ! splicer begin module_use
+    ! splicer end module_use
+    implicit none
+
+    ! splicer begin module_top
+    ! splicer end module_top
+
+    interface
+    end interface
+
+contains
+
+end module library_mod
