e06679f5b32077ba42325651c7f8a54cc9824b00
Lee Taylor <ltaylor@llnl.gov>
2019-08-14 17:14:48 -0700

    Tweek filenames created by names.yaml
    class will not create a Fortran file.
    namespace and class both create a c++ file.

--- regression/reference/names/foo.cpp
+++ regression/reference/names/foo.cpp
@@ -14,15 +14,34 @@
 // For details about use and distribution, please read LICENSE.
 //
 // #######################################################################
+#include "foo.h"
 
-// splicer begin CXX_definitions
-// Add some text from splicer
-// And another line
-// splicer end CXX_definitions
+// splicer begin class.Names.CXX_definitions
+// splicer end class.Names.CXX_definitions
 
 extern "C" {
 
-// splicer begin C_definitions
-// splicer end C_definitions
+// splicer begin class.Names.C_definitions
+// splicer end class.Names.C_definitions
+
+// void method1()
+void XXX_TES_names_method1(TES_names * self)
+{
+// splicer begin class.Names.method.method1
+    ns0::Names *SH_this = static_cast<ns0::Names *>(self->addr);
+    SH_this->method1();
+    return;
+// splicer end class.Names.method.method1
+}
+
+// void method2()
+void XXX_TES_names_method2(TES_names * self2)
+{
+// splicer begin class.Names.method.method2
+    ns0::Names *SH_this2 = static_cast<ns0::Names *>(self2->addr);
+    SH_this->method2();
+    return;
+// splicer end class.Names.method.method2
+}
 
 }  // extern "C"
