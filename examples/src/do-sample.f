2e0331bfce3722c13407da115da36db3cbbfe2b2
Lee Taylor <ltaylor@llnl.gov>
2019-09-05 20:38:50 -0700

    Add a sample using gmake

---
+++ examples/src/do-sample.f
@@ -0,0 +1,10 @@
+! Copyright (c) 2017-2019, Lawrence Livermore National Security, LLC and
+! other Shroud Project Developers.
+! See the top-level COPYRIGHT file for details.
+!
+! SPDX-License-Identifier: (BSD-3-Clause)
+
+program do_sample
+   use sample_mod
+   call worker1
+end program do_sample
