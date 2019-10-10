7b8c68542f15823297ce58b21da45e1794308c22
Lee Taylor <ltaylor@llnl.gov>
2019-10-10 16:31:31 -0700

    Add option C_API_case
    Possible values are 'lower', 'upper', 'native'.  Modifies
    C_name_scope.  An earlier update preserved the case of namespaces and
    classes when generated the C API.  By setting C_API_case to 'lower',
    the previous behavior can be restored.

---
+++ regression/reference/names/wrapClass1.hh
@@ -0,0 +1,44 @@
+// wrapClass1.hh
+// This is generated code, do not edit
+// #######################################################################
+// Copyright (c) 2017-2019, Lawrence Livermore National Security, LLC.
+//
+// Produced at the Lawrence Livermore National Laboratory
+//
+// LLNL-CODE-738041.
+//
+// All rights reserved.
+//
+// This file is part of Shroud.
+//
+// For details about use and distribution, please read LICENSE.
+//
+// #######################################################################
+/**
+ * \file wrapClass1.hh
+ * \brief Shroud generated wrapper for Class1 class
+ */
+// For C users and C++ implementation
+
+#ifndef WRAPCLASS1_HH
+#define WRAPCLASS1_HH
+
+#include "typestestnames.hh"
+
+// splicer begin namespace.CAPI.class.Class1.CXX_declarations
+// splicer end namespace.CAPI.class.Class1.CXX_declarations
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// splicer begin namespace.CAPI.class.Class1.C_declarations
+// splicer end namespace.CAPI.class.Class1.C_declarations
+
+void TES_capi_class1_member1(TES_capi_class1 * self);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // WRAPCLASS1_HH
