ef08f8e650fb494488211d6d5c2eed32329b4a00
Lee Taylor <ltaylor@llnl.gov>
2019-10-12 12:04:13 -0700

    Add test-c-namespace target

---
+++ regression/run/namespace/testc.c
@@ -0,0 +1,30 @@
+/* Copyright (c) 2017-2019, Lawrence Livermore National Security, LLC and
+ * other Shroud Project Developers.
+ * See the top-level COPYRIGHT file for details.
+ *
+ * SPDX-License-Identifier: (BSD-3-Clause)
+ */
+
+#include <wrapns.h>
+#include <wrapns_outer.h>
+
+#include <assert.h>
+
+void test_ns(void)
+{
+  NS_one();
+}
+
+void test_ns_outer(void)
+{
+  NS_outer_one();
+}
+
+int main(int argc, char *argv[])
+{
+  test_ns();
+  test_ns_outer();
+
+  return 0;
+}
+
