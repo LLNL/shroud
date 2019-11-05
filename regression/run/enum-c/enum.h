d4ca309c47bf78ac3647aa354121ca543f46684d
Lee Taylor <ltaylor@llnl.gov>
2019-11-05 14:39:33 -0800

    Add test-fortran-enum-c

---
+++ regression/run/enum-c/enum.h
@@ -0,0 +1,17 @@
+// Copyright (c) 2017-2019, Lawrence Livermore National Security, LLC and
+// other Shroud Project Developers.
+// See the top-level COPYRIGHT file for details.
+//
+// SPDX-License-Identifier: (BSD-3-Clause)
+
+#ifndef ENUM_H
+#define ENUM_H
+
+enum Color {
+  RED = 10,
+  BLUE,
+  WHITE
+};
+
+#endif // ENUM_H
+
