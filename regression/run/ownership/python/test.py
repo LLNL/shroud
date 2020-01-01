a9c826a8e71a902e4e2c330a525693fdba9a165d
Lee Taylor <ltaylor@llnl.gov>
2020-01-01 13:42:35 -0800

    Update copyright for 2020

--- regression/run/ownership/python/test.py
+++ regression/run/ownership/python/test.py
@@ -1,18 +1,11 @@
-# Copyright (c) 2017-2019, Lawrence Livermore National Security, LLC. 
+# Copyright (c) 2017-2020, Lawrence Livermore National Security, LLC and
+# other Shroud Project Developers.
+# See the top-level COPYRIGHT file for details.
 #
-# Produced at the Lawrence Livermore National Laboratory 
-#
-# LLNL-CODE-738041.
-#
-# All rights reserved. 
-#
-# This file is part of Shroud.
-#
-# For details about use and distribution, please read LICENSE.
-#
+# SPDX-License-Identifier: (BSD-3-Clause)
 # #######################################################################
 #
-# test the tutorial module
+# test the ownership module
 #
 from __future__ import print_function
 
