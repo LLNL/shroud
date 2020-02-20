f081bdacc01291d91b6e78514f0c53157ff871fb
Lee Taylor <taylor16@llnl.gov>
2020-02-20 14:35:11 -0800

    Add templates test.py

---
+++ regression/run/templates/python/testpython.c
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2017-2020, Lawrence Livermore National Security, LLC and
+ * other Shroud Project Developers.
+ * See the top-level COPYRIGHT file for details.
+ *
+ * SPDX-License-Identifier: (BSD-3-Clause)
+ * #######################################################################
+ */
+#include "Python.h"
+//#include <pytemplatesmodule.hpp>
+#include <stdio.h>
+
+#if PY_MAJOR_VERSION >= 3
+#define MODINIT  PyInit_templates
+#else
+#define MODINIT  inittemplates
+#endif
+PyMODINIT_FUNC MODINIT(void);
+
+int main(int argc, char** argv)  
+{
+    char filename[] = "test.py";
+    FILE* fp;
+
+    Py_Initialize();
+    MODINIT();
+    
+    fp = fopen(filename, "r");
+    PyRun_SimpleFile(fp, filename);
+    fclose(fp);
+    Py_Exit(0);  
+    return 0;
+}
