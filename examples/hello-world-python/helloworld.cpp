5bad26fcdeca5a2cd2c3791d5c364c69d5818610
vsoch <vsoch@users.noreply.github.com>
2021-03-03 11:04:05 -0700

    adding complete hello world python example with container
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

---
+++ examples/hello-world-python/helloworld.cpp
@@ -0,0 +1,30 @@
+// AbiSpack.cpp
+// Functions for spack to interact with libabigail (or other ABI libraries)
+
+#include <iostream>
+#include <string>
+#include "helloworld.hpp"
+
+
+namespace helloworld {
+    
+    int Person::SayHello() {
+        std::cout << "Hello!\n";
+        return 0;
+    }
+
+    int Person::NamedHello(std::string name) {
+        std::cout << "Hello " << name << "!\n";
+        return 0;
+    }
+      
+}
+
+
+int main() {
+
+    helloworld::Person person;
+    std::string name = "Dinosaur";
+    person.NamedHello(name);
+    return 0; 
+}
---
+++ examples/hello-world-python/helloworld.hpp
@@ -0,0 +1,15 @@
+// abispack.h
+#pragma once
+
+#include <iostream>
+#include <string>
+
+namespace helloworld
+{
+    class Person
+    {
+    public:
+        static int SayHello();
+        static int NamedHello(std::string name);
+    };    
+}
