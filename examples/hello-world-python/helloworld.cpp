b1b93a58a95b90229dc173dc24c0986c2df4e0b7
vsoch <vsoch@users.noreply.github.com>
2021-03-04 09:50:39 -0700

    header file did not save!
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- examples/hello-world-python/helloworld.cpp
+++ examples/hello-world-python/helloworld.cpp
@@ -1,5 +1,10 @@
 // helloworld.cpp
-// Functions for spack to interact with libabigail (or other ABI libraries)
+// Copyright (c) 2021, Lawrence Livermore National Security, LLC and
+// other Shroud Project Developers.
+// See the top-level COPYRIGHT file for details.
+//
+// SPDX-License-Identifier: (BSD-3-Clause)
+
 
 #include <iostream>
 #include <string>
