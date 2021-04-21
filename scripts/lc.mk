dff5cd40d99257ebff482f7f14bfc3ec6c1b7e17
Lee Taylor <taylor16@llnl.gov>
2021-04-20 20:20:13 -0700

    Add makefile to test using several compiler/version
    Each test is put into its own directory under build/.../run

---
+++ scripts/lc.mk
@@ -0,0 +1,64 @@
+#
+# Test with various compilers at Livermore Computing
+#
+# LC installs compilers in a consistent pattern which allows gmake
+# pattern rules to be used.
+
+gccdir = /usr/tce/packages/gcc
+inteldir = /usr/tce/packages/intel
+pgidir = /usr/tce/packages/pgi
+
+target = test-fortran-tutorial
+
+all : gcc intel pgi
+.PHONY : all
+
+clean :
+	$(MAKE) test-clean
+.PHONY : clean
+
+gcc : \
+  gcc-4.9.3 \
+  gcc-6.1.0 \
+  gcc-7.3.0 \
+  gcc-8.3.1 \
+  gcc-9.3.1 \
+  gcc-10.2.1
+.PHONY : gcc
+
+gcc-% :
+	$(MAKE) $(target) testdir=$@ compiler=gcc \
+	CC=$(gccdir)/$@/bin/gcc \
+	CXX=$(gccdir)/$@/bin/g++ \
+	FC=$(gccdir)/$@/bin/gfortran
+#.PHONY : gcc-9.3.1
+
+intel : \
+  intel-14.0.3 \
+  intel-15.0.6 \
+  intel-16.0.4 \
+  intel-17.0.2 \
+  intel-18.0.2 \
+  intel-19.1.2 \
+  intel-2021.2
+
+intel-% :
+	$(MAKE) $(target) testdir=$@ compiler=intel \
+	CC=$(inteldir)/$@/bin/icc \
+	CXX=$(inteldir)/$@/bin/icpc \
+	FC=$(inteldir)/$@/bin/ifort
+
+#  pgi-16.9  missing -cpp flag
+pgi : \
+ pgi-17.10 \
+ pgi-18.5 \
+ pgi-19.7 \
+ pgi-20.1 \
+ pgi-21.1 \
+
+pgi-% :
+	$(MAKE) $(target) testdir=$@ compiler=pgi \
+	CC=$(pgidir)/$@/bin/pgcc \
+	CXX=$(pgidir)/$@/bin/pgc++ \
+	FC=$(pgidir)/$@/bin/pgf90
+
