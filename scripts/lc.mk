d4c21489296b8329be1a9701af887bae719d39ca
Lee Taylor <taylor16@llnl.gov>
2021-04-23 23:11:03 -0700

    Mark more targets as PHONY
    Now, 'make test-fortran-xxx' compains about the target not existing
    instead of missing Makefile in regression/xxx directory.
    
    Plus, these targets do not create files with the name of the target
    so they are PHONY.
    Create fortran-test-list variable so that the pattern targets can
    be PHONY:
    
    .PHONY : $(fortran-test-list)
    $(fortran-test-list) : test-fortran-% : fortran-%
    
    Note that target 'fortran-% is not yet PHONY.

--- scripts/lc.mk
+++ scripts/lc.mk
@@ -20,23 +20,26 @@ clean :
 	$(MAKE) test-clean
 .PHONY : clean
 
-gcc : \
+gcc-list = \
   gcc-4.9.3 \
   gcc-6.1.0 \
   gcc-7.3.0 \
   gcc-8.3.1 \
   gcc-9.3.1 \
   gcc-10.2.1
-.PHONY : gcc
 
-gcc-% :
+.PHONY : gcc
+gcc : $(gcc-list)
+
+.PHONY : $(gcc-list)
+$(gcc-list) : gcc-% :
 	$(MAKE) $(makeargs) testdir=$@ compiler=gcc \
 	CC=$(gccdir)/$@/bin/gcc \
 	CXX=$(gccdir)/$@/bin/g++ \
 	FC=$(gccdir)/$@/bin/gfortran
 #.PHONY : gcc-9.3.1
 
-intel : \
+intel-list = \
   intel-14.0.3 \
   intel-15.0.6 \
   intel-16.0.4 \
@@ -45,21 +48,29 @@ intel : \
   intel-19.1.2 \
   intel-2021.2
 
-intel-% :
+.PHONY : intel
+intel : $(intel-list)
+
+.PHONY : $(intel-list)
+$(intel-list) : intel-% :
 	$(MAKE) $(makeargs) testdir=$@ compiler=intel \
 	CC=$(inteldir)/$@/bin/icc \
 	CXX=$(inteldir)/$@/bin/icpc \
 	FC=$(inteldir)/$@/bin/ifort
 
 #  pgi-16.9  missing -cpp flag
-pgi : \
+pgi-list = \
  pgi-17.10 \
  pgi-18.5 \
  pgi-19.7 \
  pgi-20.1 \
  pgi-21.1 \
 
-pgi-% :
+.PHONY : pgi
+pgi : $(pgi-list)
+
+.PHONY : $(pgi-list)
+$(pgi-list) : pgi-% :
 	$(MAKE) $(makeargs) testdir=$@ compiler=pgi \
 	CC=$(pgidir)/$@/bin/pgcc \
 	CXX=$(pgidir)/$@/bin/pgc++ \
