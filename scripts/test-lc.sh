24f487c18e9d6157a2e9b761065637efb100474e
Lee Taylor <taylor16@llnl.gov>
2021-04-28 15:53:25 -0700

    Add scripts/summary.py
    Used to print a summary of compiler test coverage.

--- scripts/test-lc.sh
+++ scripts/test-lc.sh
@@ -13,6 +13,8 @@ srun make -f scripts/lc.mk target=test-fortran all -j
 # Compile Python tests with gcc
 srun make -f scripts/lc.mk target=test-python python -j
 
+scripts/summary.py build/regression/run
+
 date
 
 # Create summary
