b88e4639d43f452b5517ed6d7659bec940617ff5
Lee Taylor <taylor16@llnl.gov>
2024-10-21 17:49:49 -0700

    Use abstract declarator with descriptors
    Consistent with how function arguments statement fields are created.
    
    - py_descr_bool_scalar
    + py_descr_bool
    
    - py_descr_native_*_list
    + py_descr_native*_list

---
+++ regression/run/shared/shared.hpp
@@ -0,0 +1,46 @@
+// Copyright (c) 2017-2024, Lawrence Livermore National Security, LLC and
+// other Shroud Project Developers.
+// See the top-level COPYRIGHT file for details.
+//
+// SPDX-License-Identifier: (BSD-3-Clause)
+//
+// shared.hpp - wrapped routines
+//
+
+#ifndef SHARED_HPP
+#define SHARED_HPP
+
+#include <memory>
+
+class Object
+{
+public:
+    Object() 
+    {
+    }
+
+    ~Object()
+    {
+    }
+
+    std::shared_ptr<Object>* createChildA(void)
+    {
+        childA = std::shared_ptr<Object>(new Object);
+        return &childA;
+    }
+    std::shared_ptr<Object>* createChildB(void)
+    {
+        childB = std::shared_ptr<Object>(new Object);
+        return &childB;
+    }
+    void replaceChildB(std::shared_ptr<Object>* child)
+    {
+        childB = *child;
+    }
+
+private:
+    std::shared_ptr<Object> childA;
+    std::shared_ptr<Object> childB;
+};
+
+#endif // SHARED_HPP
