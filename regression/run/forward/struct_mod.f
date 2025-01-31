9f238d21fa7e4f10fd6dda543c0c06f18fdf15fa
Lee Taylor <taylor16@llnl.gov>
2025-01-31 13:09:02 -0800

    Update copyright for 2025

--- regression/run/forward/struct_mod.f
+++ regression/run/forward/struct_mod.f
@@ -1,4 +1,4 @@
-! Copyright (c) 2017-2024, Lawrence Livermore National Security, LLC and
+! Copyright (c) 2017-2025, Lawrence Livermore National Security, LLC and
 ! other Shroud Project Developers.
 ! See the top-level COPYRIGHT file for details.
 !
--- regression/run/forward/wrapstruct.h
+++ regression/run/forward/wrapstruct.h
@@ -1,6 +1,6 @@
 // wrapstruct.h
 
-// Copyright (c) 2017-2024, Lawrence Livermore National Security, LLC and
+// Copyright (c) 2017-2025, Lawrence Livermore National Security, LLC and
 // other Shroud Project Developers.
 // See the top-level COPYRIGHT file for details.
 //
